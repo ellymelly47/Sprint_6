7d8366d79d17af362aeffaab3c109229e05bcce0
Evgeniya Berdnikova <ellymelly47@yandex.ru>
2023-10-16 00:30:19 +0300

    removed_type_hints

--- pages/base_page.py
+++ pages/base_page.py
@@ -8,24 +8,24 @@ class BasePage:
         self.driver = driver
 
     @allure.step('Ожидаем, пока элемент становится видимым, и кликаем на него')
-    def click_on_element(self, sec: int, element_locator):
+    def click_on_element(self, sec, element_locator):
         element = WebDriverWait(self.driver, sec).until(expected_conditions.visibility_of_element_located(
             element_locator))
         element.click()
 
     @allure.step('Ищем элемент, ожидая, пока он становится видимым')
-    def find_the_element(self, sec: int, element_locator):
+    def find_the_element(self, sec, element_locator):
         element = WebDriverWait(self.driver, sec).until(expected_conditions.visibility_of_element_located(
             element_locator))
         return element
 
     @allure.step('Ждем, пока указанный элемент станет видимым')
-    def wait_until_element_visibility(self, sec: int, element_locator):
+    def wait_until_element_visibility(self, sec, element_locator):
         WebDriverWait(self.driver, sec).until(expected_conditions.visibility_of_element_located(
             element_locator))
 
     @allure.step('Ждем, пока указанный элемент перестанет быть видимым')
-    def wait_until_element_invisibility(self, sec: int, element_locator):
+    def wait_until_element_invisibility(self, sec, element_locator):
         WebDriverWait(self.driver, sec).until(expected_conditions.invisibility_of_element_located(
             element_locator))
 
